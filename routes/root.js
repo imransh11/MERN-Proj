3530a1ca1fd184e243726f315fe899de32ac3d78
Imran Shaukat <imransh11@gmail.com>
2025-04-13 07:50:56 -0400

    404 page - regex v5 update

--- routes/root.js
+++ routes/root.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-router.get('/'||'/index(.html)?', (req, res) => {
+router.get(['/', '/index{.html}'], (req, res) => {
     res.sendFile(path.join(__dirname, "..", 'views', 'index.html'))
 })
+
+// '^/$', '/index{.html}'
 
 module.exports = router
